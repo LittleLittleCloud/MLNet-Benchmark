dc9ba5c9c6b77da424b308798cb27daffa4cb402
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-01-12 17:58:53 -0800

    update

--- MLNetBenchmark/MLNetBenchmark/Program.cs
+++ MLNetBenchmark/MLNetBenchmark/Program.cs
@@ -1,6 +1,6 @@
-// ﻿await ObjectDetectionBenchmark.RunCatObjectDetectionVottAsync();
+// await ObjectDetectionBenchmark.RunCatObjectDetectionVottAsync();
 await ObjectDetectionBenchmark.RunCatObjectDetectionCoCoAsync();
-await ForecastingBenchmark.RunBitcoinForecastingAsync();
+// await ForecastingBenchmark.RunBitcoinForecastingAsync();
 await RegressionBenchmark.RunTaxiFareAsync();
 await BinaryClassificationBenchmark.RunTitanicAsync();
 await RecommendationBenchmark.RunMovieRecommendationAsync();
