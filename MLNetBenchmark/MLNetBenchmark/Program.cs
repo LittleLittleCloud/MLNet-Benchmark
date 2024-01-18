78c2a07d02221dd1e3e56342861978ca1588daf3
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-01-17 17:19:59 -0800

    update ci

--- MLNetBenchmark/MLNetBenchmark/Program.cs
+++ MLNetBenchmark/MLNetBenchmark/Program.cs
@@ -1,14 +1,15 @@
 using System.Runtime.InteropServices;
+if (RuntimeInformation.ProcessArchitecture != Architecture.Arm64)
+{
+    await TextClassificationBenchmark.RunWikipediaAsync();
+    await ImageClassificationBenchmark.RunWeatherDataAsync();
+    await ObjectDetectionBenchmark.RunCatObjectDetectionVottAsync();
+    await ObjectDetectionBenchmark.RunCatObjectDetectionCoCoAsync();
+    await ForecastingBenchmark.RunBitcoinForecastingAsync();
+}
 
 await BinaryClassificationBenchmark.RunTitanicAsync();
 await RecommendationBenchmark.RunMovieRecommendationAsync();
 await RegressionBenchmark.RunTaxiFareAsync();
 
-if (RuntimeInformation.ProcessArchitecture != Architecture.Arm64)
-{
-    await ImageClassificationBenchmark.RunWeatherDataAsync();
-    await TextClassificationBenchmark.RunWikipediaAsync();
-    await ObjectDetectionBenchmark.RunCatObjectDetectionVottAsync();
-    await ObjectDetectionBenchmark.RunCatObjectDetectionCoCoAsync();
-    await ForecastingBenchmark.RunBitcoinForecastingAsync();
-}
+
